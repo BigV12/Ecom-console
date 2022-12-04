c3d811ea3e2a3481b1c91d813cbf194ce0d049b4
BigV12 <108405584+BigV12@users.noreply.github.com>
2022-12-04 09:52:25 +0100

    Add files via upload

---
+++ Ecom console.js
@@ -0,0 +1,27 @@
+
+// list of items available in the store
+
+let person = [ list1= {Item:"Coke", Price:"$45", description:"Carbonated drink for your taste"},
+
+ list3={Item:"Bugar", Price:"$12", description:"Good for your stomach and mouth"},
+
+ list4={Item:"Sharwama", Price:"55", description:"Food for girls"},
+
+ list5={Item:"Pizza", Price:"18", description:"E cost shaa"},
+
+ list6={Item:"Gala", Price:"$26", description:"Food for traffic and holdup"},
+
+]
+
+
+//Cart
+let newCart = [
+
+]
+
+//adding an item to cart
+
+newCart.push(person[1])
+
+// removing an item from the cart
+newCart.pop(person[1])
